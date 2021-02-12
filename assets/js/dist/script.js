96ebf08c72f90b867126686e987841510f4ad1e9
Krushil <krushilnaik96@gmail.com>
2021-02-12 18:39:13 -0500

    print relevant response data onto page

--- assets/js/dist/script.js
+++ assets/js/dist/script.js
@@ -2,21 +2,49 @@
 var searchButton = document.querySelector("button");
 var searchField = document.querySelector("input");
 var API_KEY = "81e9f56d1663d1d6f860c7b97883e905";
+function toDateString(unixTime) {
+    return (new Date(unixTime * 1000)).toLocaleDateString();
+}
 searchButton.addEventListener("click", function () {
-    var cityName = searchField.value;
-    console.log("Requested data for city '" + cityName + "'");
-    var initialRequestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + API_KEY;
+    var query = searchField.value;
+    console.log("Requested data for city '" + query + "'");
+    var initialRequestURL = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + API_KEY;
+    console.log(initialRequestURL);
     // var requestURL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&appid=${API_KEY}&units=imperial`;
     // console.log(initialRequestURL);
     var ONE_CALL = "https://api.openweathermap.org/data/2.5/onecall";
+    var iconBaseURL = "http://openweathermap.org/img/w";
+    var cityName;
     // The UV Index is only found in the One Call API
     // which uses lat and lon instead of city name
     // for now, fetch those separately via arbitrary API
     // and pass those into the "main" fetch
-    fetch(initialRequestURL).then(function (response) { return response.json(); }).then(function (data) { return [data.coord.lat, data.coord.lon]; }).then(function (coords) {
+    fetch(initialRequestURL).then(function (response) { return response.json(); }).then(function (data) {
+        cityName = data.name;
+        return [data.coord.lat, data.coord.lon];
+    }).then(function (coords) {
         fetch(ONE_CALL + ("?lat=" + coords[0] + "&lon=" + coords[1] + "&exclude=hourly,minutely,alerts&units=imperial&appid=" + API_KEY)).then(function (response) { return response.json(); }).then(function (data) {
-            // let responseData: ResponseModel = data;
-            console.log(data);
+            // set up an expected structure for the parsed JSON
+            // solely for better code completion
+            var _data = data;
+            console.log(cityName);
+            var currentWeather = _data.current;
+            var dailyWeather = _data.daily;
+            var cards = document.querySelectorAll(".card");
+            var currentCity = document.querySelector("#city");
+            var currentTemperature = document.querySelector("#current-temperature");
+            var currentHumidity = document.querySelector("#current-humidity");
+            var currentWind = document.querySelector("#current-wind");
+            var currentUVIndex = document.querySelector("#current-uv");
+            currentCity.innerHTML = cityName + " (" + toDateString(currentWeather.dt) + ")";
+            currentTemperature.innerHTML = currentWeather.temp + " \u00B0F";
+            currentHumidity.innerHTML = currentWeather.humidity + "%";
+            currentWind.innerHTML = currentWeather.wind_speed + " MPH";
+            currentUVIndex.innerHTML = currentWeather.uvi.toLocaleString();
+            for (var i = 0; i < cards.length; i++) {
+                var currentCard = dailyWeather[i + 1];
+                cards[i].innerHTML = "\n\t\t\t\t\t\t\t<div>" + toDateString(currentCard.dt) + "</div>\n\t\t\t\t\t\t\t<img src=\"" + iconBaseURL + "/" + currentCard.weather[0].icon + ".png\" alt=\"" + currentWeather.weather[0].description + "\">\n\t\t\t\t\t\t\t<div>Temp: " + currentCard.temp.day + " \u00B0F</div>\n\t\t\t\t\t\t\t<div>Humidity: " + currentCard.humidity + "%</div>\n\t\t\t\t\t\t";
+            }
         });
     });
 });
--- assets/js/script.ts
+++ assets/js/script.ts
@@ -6,8 +6,9 @@ interface DayWeatherModel {
 	dt: number;
 	wind_speed: number;
 	uvi: number;
+	temp: {day: number;};
 	humidity: number;
-	weather: {icon: string; description: string;};
+	weather: {icon: string; description: string;}[];
 }
 
 interface ResponseModel {
@@ -17,16 +18,24 @@ interface ResponseModel {
 
 const API_KEY = "81e9f56d1663d1d6f860c7b97883e905";
 
+function toDateString(unixTime: number) {
+	return (new Date(unixTime * 1000)).toLocaleDateString();
+}
+
 searchButton.addEventListener("click", function() {
-	const cityName = searchField.value;
-	console.log(`Requested data for city '${cityName}'`);
+	const query = searchField.value;
+	console.log(`Requested data for city '${query}'`);
 
+	var initialRequestURL: string = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${API_KEY}`;
+	console.log(initialRequestURL);
 
-	var initialRequestURL: string = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
 	// var requestURL = `https://api.openweathermap.org/data/2.5/forecast/daily?q=${cityName}&appid=${API_KEY}&units=imperial`;
 	// console.log(initialRequestURL);
 
 	var ONE_CALL = "https://api.openweathermap.org/data/2.5/onecall";
+
+	var iconBaseURL = "http://openweathermap.org/img/w";
+	let cityName: string;
 
 	// The UV Index is only found in the One Call API
 	// which uses lat and lon instead of city name
@@ -35,15 +44,49 @@ searchButton.addEventListener("click", function() {
 	fetch(initialRequestURL).then(
 		response => response.json()
 	).then(
-		data => [data.coord.lat, data.coord.lon]
+		data => {
+			cityName = data.name;
+			return [data.coord.lat, data.coord.lon];
+		}
 	).then (
 		coords => {
 			fetch(ONE_CALL + `?lat=${coords[0]}&lon=${coords[1]}&exclude=hourly,minutely,alerts&units=imperial&appid=${API_KEY}`).then(
 				response => response.json()
 			).then(
 				data => {
-					// let responseData: ResponseModel = data;
-					console.log(data);
+					// set up an expected structure for the parsed JSON
+					// solely for better code completion
+					let _data: ResponseModel = data;
+
+					console.log(cityName);
+					
+
+					let currentWeather = _data.current;
+					let dailyWeather = _data.daily;
+
+					let cards = document.querySelectorAll(".card");
+					let currentCity = document.querySelector("#city");
+					let currentTemperature = document.querySelector("#current-temperature");
+					let currentHumidity = document.querySelector("#current-humidity");
+					let currentWind = document.querySelector("#current-wind");
+					let currentUVIndex = document.querySelector("#current-uv");
+
+					currentCity.innerHTML = `${cityName} (${toDateString(currentWeather.dt)})`;
+					currentTemperature.innerHTML = `${currentWeather.temp} °F`;
+					currentHumidity.innerHTML = `${currentWeather.humidity}%`;
+					currentWind.innerHTML = `${currentWeather.wind_speed} MPH`;
+					currentUVIndex.innerHTML = currentWeather.uvi.toLocaleString();
+
+					for (let i = 0; i < cards.length; i++) {
+						const currentCard = dailyWeather[i+1];
+
+						cards[i].innerHTML = `
+							<div>${toDateString(currentCard.dt)}</div>
+							<img src="${iconBaseURL}/${currentCard.weather[0].icon}.png" alt="${currentWeather.weather[0].description}">
+							<div>Temp: ${currentCard.temp.day} °F</div>
+							<div>Humidity: ${currentCard.humidity}%</div>
+						`;
+					}
 				}
 			)
 		}
